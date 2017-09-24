861724729f80a85510928c82029837f5820a50e6
roboticafacil <roboticafacil.es@gmail.com>
2017-09-24 18:42:20 +0200

    modified:   qblockswebview.cpp 	modified:   qblockswebview.h

--- src/qblockswebview.cpp
+++ src/qblockswebview.cpp
@@ -2,11 +2,43 @@
 
 QBlocksWebView::QBlocksWebView() {
 }
-QBlocksWebView::QBlocksWebView(QWidget *parent)
-     {
+QBlocksWebView::QBlocksWebView(QWidget *parent): QWebEngineView(parent), child_(nullptr)
+{
 }
 
-void QBlocksWebView::wheelEvent(QWheelEvent *event) {
+bool QBlocksWebView::eventFilter(QObject *obj, QEvent *ev)
+{
+        // emit delegatePaint on paint event of the last added QOpenGLWidget child
+        if (obj == child_ && ev->type() == QEvent::Wheel) {
+            QWheelEvent *we = static_cast<QWheelEvent*>(ev);
+            // do something with paint event
+            // ...
+            // or just emit signal to notify other objects
+            emit delegateWheel(we);
+        }
+
+        return QWebEngineView::eventFilter(obj, ev);
+}
+
+bool QBlocksWebView::event(QEvent * ev)
+    {
+        if (ev->type() == QEvent::ChildAdded) {
+            QChildEvent *child_ev = static_cast<QChildEvent*>(ev);
+
+            // there is also QObject child that should be ignored here;
+            // use only QOpenGLWidget child
+            QOpenGLWidget *w = qobject_cast<QOpenGLWidget*>(child_ev->child());
+            if (w) {
+                child_ = w;
+                w->installEventFilter(this);
+            }
+        }
+        return QWebEngineView::event(ev);
+    }
+
+
+void QBlocksWebView::delegateWheel(QWheelEvent *event)
+{
     // Capture mouse wheel events.
     // Zoom the WebView
     float scale = event->delta() / 120.0 / 10.0;
