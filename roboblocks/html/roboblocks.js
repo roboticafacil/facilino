122f71e1a5486f59f2083501547ddc2e3e6470ed
roboticafacil <roboticafacil.es@gmail.com>
2018-12-12 13:47:41 +0100

    modified:   roboblocks/html/Facilino.html 	modified:   roboblocks/html/FacilinoTranslate.html 	modified:   roboblocks/html/doc/ca-ES/about.html 	modified:   roboblocks/html/doc/en-GB/about.html 	modified:   roboblocks/html/doc/es-ES/about.html 	new file:   roboblocks/html/doc/examples/dyor_bPED_demo.bly 	new file:   roboblocks/html/doc/examples/dyor_bPED_demo_simple.bly 	modified:   roboblocks/html/lang/facilino_pt-PT.json 	new file:   roboblocks/html/lang/facilino_sv-SE.json 	modified:   roboblocks/html/roboblocks.js 	modified:   src/aboutdialog.ui

--- roboblocks/html/roboblocks.js
+++ roboblocks/html/roboblocks.js
@@ -6012,31 +6012,7 @@
             }
         };
         // Source: src/blocks/text_substring/text_substring.js
-        Blockly.Arduino.text_substring = function() {
-            var string1 = Blockly.Arduino.valueToCode(this, 'STRING1', Blockly.Arduino.ORDER_NONE);
-            var from = Blockly.Arduino.valueToCode(this, 'FROM', Blockly.Arduino.ORDER_NONE);
-            var to = Blockly.Arduino.valueToCode(this, 'TO', Blockly.Arduino.ORDER_NONE);
-            var code = '';
-            var a = RoboBlocks.findPinMode(string1);
-            code += a['code'];
-            string1 = a['pin'];
-
-            a = RoboBlocks.findPinMode(from);
-            code += a['code'];
-            from = a['pin'];
-
-            a = RoboBlocks.findPinMode(to);
-            code += a['code'];
-            to = a['pin'];
-
-            code += JST['text_substring']({
-                'string1': string1,
-                'from': from,
-                'to': to
-            });
-
-            return [code, Blockly.Arduino.ORDER_ATOMIC];
-        };
+        
 		
 		Blockly.Arduino.text_lower = function() {
             // String length.
@@ -6104,6 +6080,32 @@
                 this.setOutput(true, String);
                 this.setTooltip(RoboBlocks.locales.getKey('LANG_TEXT_LENGTH_UPPER_TOOLTIP'));
             }
+        };
+		
+		Blockly.Arduino.text_substring = function() {
+            var string1 = Blockly.Arduino.valueToCode(this, 'STRING1', Blockly.Arduino.ORDER_NONE);
+            var from = Blockly.Arduino.valueToCode(this, 'FROM', Blockly.Arduino.ORDER_NONE);
+            var to = Blockly.Arduino.valueToCode(this, 'TO', Blockly.Arduino.ORDER_NONE);
+            var code = '';
+            var a = RoboBlocks.findPinMode(string1);
+            code += a['code'];
+            string1 = a['pin'];
+
+            a = RoboBlocks.findPinMode(from);
+            code += a['code'];
+            from = a['pin'];
+
+            a = RoboBlocks.findPinMode(to);
+            code += a['code'];
+            to = a['pin'];
+
+            code += JST['text_substring']({
+                'string1': string1,
+                'from': from,
+                'to': to
+            });
+
+            return [code, Blockly.Arduino.ORDER_ATOMIC];
         };
 
         Blockly.Blocks.text_substring = {
